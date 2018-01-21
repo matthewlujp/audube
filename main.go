08171bd56f7059fded1545c3498cc6b9ad2c0a14
Matthew Ishige <matthewlujp@gmail.com>
2018-01-21 15:43:21 +0900

    Directly feed url to ffmpeg

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"path/filepath"
 
 	"github.com/labstack/echo"
 )
@@ -28,40 +27,17 @@ func init() {
 	flag.Parse()
 }
 
-func main() {
-	if err := os.MkdirAll(videosDir, 0777); err != nil && !os.IsExist(err) {
-		logger.Fatal(err)
-	}
-	if err := os.MkdirAll(audiosDir, 0777); err != nil && !os.IsExist(err) {
-		logger.Fatal(err)
-	}
-
-	e := echo.New()
-	e.GET("/:videoID", convertHandler)
-	e.Logger.Fatal(e.Start(":1234"))
-}
-
 func convertHandler(c echo.Context) error {
 	videoID := c.Param("videoID")
 
 	audioPath := path.Join(audiosDir, fmt.Sprintf("%s.mp3", videoID))
 	if !exists(audioPath) {
-		tmpFile, err := ioutil.TempFile("", "audube_")
+		videoURL, err := getVideoURL(videoID)
 		if err != nil {
-			logger.Print(err)
-			return err
-		}
-		defer os.Remove(tmpFile.Name())
-		tmpFilePath, err := filepath.Abs(tmpFile.Name())
-		if err != nil {
-			logger.Print(err)
-			return err
-		}
-
-		if err := downloadVideo(videoID, tmpFilePath); err != nil {
 			return c.String(http.StatusInternalServerError, fmt.Sprint(err))
 		}
-		if err := convert(tmpFilePath, audioPath); err != nil {
+
+		if err := convert(videoURL, audioPath); err != nil {
 			return c.String(http.StatusInternalServerError, fmt.Sprint(err))
 		}
 	}
@@ -84,4 +60,17 @@ func exists(path string) bool {
 		return false
 	}
 	return true
+}
+
+func main() {
+	if err := os.MkdirAll(videosDir, 0777); err != nil && !os.IsExist(err) {
+		logger.Fatal(err)
+	}
+	if err := os.MkdirAll(audiosDir, 0777); err != nil && !os.IsExist(err) {
+		logger.Fatal(err)
+	}
+
+	e := echo.New()
+	e.GET("/:videoID", convertHandler)
+	e.Logger.Fatal(e.Start(":1234"))
 }
--- media.go
+++ media.go
@@ -1,30 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os/exec"
-
-	"github.com/kkdai/youtube"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func downloadVideo(videoID, filePath string) error {
-	logger.Printf("download to file: %s", filePath)
+const (
+	youtubeInfoURL = "https://www.youtube.com/get_video_info?video_id="
+	targetType     = "video/mp4"
+)
 
-	y := youtube.NewYoutube(*verbose)
-	if err := y.DecodeURL(fmt.Sprintf("https://www.youtube.com/watch?v=%s", videoID)); err != nil {
-		logger.Printf("decode url %s", err)
-		return err
-	}
-	if err := y.StartDownload(filePath); err != nil {
-		logger.Printf("download, %s", err)
-		return err
-	}
-	return nil
+var (
+	qualities            = []string{"small", "medium", "hd720"}
+	errRedirectAttempted = errors.New("redirect")
+)
+
+type stream struct {
+	quality   string
+	mediaType string
+	url       string // video/mp4, video/webcam, etc.
 }
 
-func convert(videoPath, audioPath string) error {
-	cmd := exec.Command("ffmpeg", "-i", videoPath, "-acodec", "libmp3lame", "-ab", "256k", audioPath)
+type contentInfo struct {
+	id             string
+	title          string
+	author         string
+	downloadedTime int64 // Unix time
+	thumbnailURL   string
+	keywords       []string
+	lengthSeconds  int64
+	streams        []stream
+}
+
+func getVideoURL(videoID string) (string, error) {
+	videoInfo, err := getVideoInfo(videoID)
+	if err != nil {
+		return "", err
+	}
+
+	targetURL, err := getDownloadableURL(videoInfo.streams)
+	if err != nil {
+		return "", err
+	}
+	return targetURL, nil
+}
+
+func convert(videoURL, audioPath string) error {
+	cmd := exec.Command("ffmpeg", "-ss", "30", "-i", videoURL, "-acodec", "libmp3lame", "-ab", "256k", "-f", "mp3", audioPath)
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
@@ -49,4 +79,156 @@ func convert(videoPath, audioPath string) error {
 	}
 
 	return nil
+}
+
+func getVideoInfo(id string) (*contentInfo, error) {
+	infoURL := youtubeInfoURL + id
+	res, err := http.Get(infoURL)
+	if err != nil {
+		logger.Printf("retreive video %s info, %s", id, err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		logger.Print(err)
+		return nil, err
+	}
+
+	parsedInfo, err := url.ParseQuery(string(body))
+	if err != nil {
+		logger.Print(err)
+		return nil, err
+	}
+
+	status, ok := parsedInfo["status"]
+	if !ok {
+		err = fmt.Errorf("no response status found in the server's answer")
+		logger.Print(err)
+		return nil, err
+	}
+	if status[0] == "fail" {
+		reason, ok := parsedInfo["reason"]
+		if ok {
+			err = fmt.Errorf("'fail' response status found in the server's answer, reason: '%s'", reason[0])
+			logger.Print(err)
+		} else {
+			logger.Print(err)
+			err = errors.New(fmt.Sprint("'fail' response status found in the server's answer, no reason given"))
+		}
+		return nil, err
+	}
+	if status[0] != "ok" {
+		err = fmt.Errorf("non-success response status found in the server's answer (status: '%s')", status)
+		logger.Print(err)
+		return nil, err
+	}
+
+	lengthSeconds, err := strconv.Atoi(parsedInfo["length_seconds"][0])
+	if err != nil {
+		lengthSeconds = 0
+	}
+	streams, err := parseVideoStreams(parsedInfo["url_encoded_fmt_stream_map"][0])
+	if err != nil {
+		return nil, err
+	}
+
+	info := &contentInfo{
+		id:             id,
+		title:          parsedInfo["title"][0],
+		author:         parsedInfo["author"][0],
+		downloadedTime: time.Now().Unix(),
+		thumbnailURL:   parsedInfo["thumbnail_url"][0],
+		keywords:       parsedInfo["keywords"],
+		lengthSeconds:  (int64)(lengthSeconds),
+		streams:        streams,
+	}
+	return info, nil
+}
+
+func parseVideoStreams(strStreams string) ([]stream, error) {
+	// read each stream
+	streamsList := strings.Split(strStreams, ",")
+	var streams []stream
+
+	for streamPos, streamRaw := range streamsList {
+		streamQry, err := url.ParseQuery(streamRaw)
+		if err != nil {
+			log.Printf("An error occured while decoding one of the video's stream's information: stream %d: %s\n", streamPos, err)
+			continue
+		}
+		streams = append(streams, stream{
+			quality:   streamQry["quality"][0],
+			mediaType: streamQry["type"][0],
+			url:       streamQry["url"][0],
+		})
+	}
+	return streams, nil
+}
+
+// getDownloadableURL obtains downloadable url from Youtube
+// type video/mp4 is douwnloaded
+// seek from lower resolution (small > medium > hd720)
+func getDownloadableURL(streams []stream) (string, error) {
+	var targetStream *stream
+
+	for _, quality := range qualities {
+		for i, s := range streams {
+			if s.quality == quality && strings.Contains(s.mediaType, targetType) {
+				targetStream = &streams[i]
+				break
+			}
+		}
+	}
+	if targetStream == nil {
+		err := errors.New("suported stream not found")
+		logger.Print(err)
+		return "", err
+	}
+
+	// Download video
+	targetURL := targetStream.url
+	redirectionCounter := 0
+	reqAttempMax := 10
+
+	for {
+		client := &http.Client{
+			Timeout: time.Duration(3) * time.Second,
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return errRedirectAttempted
+			},
+		}
+		logger.Printf("request to %s", targetURL)
+		res, err := client.Head(targetURL)
+		if urlErr, ok := err.(*url.Error); ok && urlErr.Err == errRedirectAttempted {
+			if redirectionCounter > reqAttempMax {
+				err = errors.New("exceeded redirection maximum attemps")
+				logger.Print(err)
+				return "", err
+			}
+
+			loc, locErr := res.Location()
+			if locErr != nil {
+				logger.Print(err)
+				return "", err
+			}
+			targetURL = loc.RawPath
+			redirectionCounter++
+			continue
+		} else if err != nil {
+			logger.Printf("header request, %s", err)
+			return "", err
+		}
+
+		if res.StatusCode == http.StatusOK {
+			break
+		} else {
+			err := fmt.Errorf("header response status: %s", res.Status)
+			logger.Print(err)
+			return "", err
+		}
+	}
+	logger.Printf("target url: %s", targetURL)
+	return targetURL, nil
 }
